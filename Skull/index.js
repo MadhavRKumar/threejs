f6e353ee710a0b921b68135f19fc65fb8be27509
unknown <madhavrkumar@gmail.com>
2019-01-04 21:14:54 -0500

    dithering?

--- Skull/index.js
+++ Skull/index.js
@@ -3,14 +3,22 @@
         renderer,
         canvas,
         scene,
+        quadScene,
         camera,
+        orthoCamera,
+        baseTexture,
         start = Date.now(),
         fov = 30,
         controls,
+        ditherMaterial,
+        quad,
         material,
         heartList = [],
         heartMaterial,
-        capturer = new CCapture({ format: 'gif', workersPath: "../three/" });
+        capturer = new CCapture({ format: 'gif', workersPath: "../three/" }),
+        pink = new THREE.Color(0xFFB6C1),
+        darkGrey = new THREE.Color(0x111111),
+        palette = [darkGrey, pink];
 
 
 
@@ -33,13 +41,24 @@
         // grab the container from the DOM
         container = document.getElementById("container");
 
-        // create a scene
+        // create the renderer and attach it to the DOM
+        renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.setPixelRatio(window.devicePixelRatio);
+
+        container.appendChild(canvas = renderer.domElement);
+
+
+        // create a scene for 3D objects
         scene = new THREE.Scene();
         scene.background = new THREE.Color(0xffffff);
 
+        // create scene for post-processing
+        quadScene = new THREE.Scene();
+
 
         // create a camera the size of the browser window
-        // and place it 100 units away, looking towards the center of the scene
+        // and place it 10000 units away, looking towards the center of the scene
         camera = new THREE.PerspectiveCamera(
             fov,
             window.innerWidth / window.innerHeight,
@@ -47,16 +66,24 @@
             10000
         );
 
+
+        // create orthographic camera for post-processing
+        orthoCamera = new THREE.OrthographicCamera(1 / -2, 1 / 2, 1 / 2, 1 / -2, .00001, 1000);
+
+
         controls = new THREE.OrbitControls(camera);
 
         //controls.update() must be called after any manual changes to the camera's transform
         camera.position.z = 50;
         controls.update();
 
+        // set up texture
+        baseTexture = new THREE.WebGLRenderTarget(window.innerWidth, window.innerHeight, { minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter, format: THREE.RGBFormat });
+
         // create a pink toon material for skelly man
         material = new THREE.ShaderMaterial({
             uniforms: {
-                ambientColor: { value: new THREE.Color(0xFFB6C1) },
+                ambientColor: { value: pink },
                 ambientStrength: { value: 0.2 },
                 lightDir: { value: new THREE.Vector3() }
             },
@@ -68,7 +95,7 @@
         // so that i can change it up if I wish
         heartMaterial = new THREE.ShaderMaterial({
             uniforms: {
-                ambientColor: { value: new THREE.Color(0xFFB6C1) },
+                ambientColor: { value: pink},
                 ambientStrength: { value: 0.2 },
                 lightDir: { value: new THREE.Vector3() },
             },
@@ -76,9 +103,27 @@
             fragmentShader: document.getElementById("fragmentShader").textContent
         });
 
+        // material to apply to quad for post-processing
+        ditherMaterial = new THREE.ShaderMaterial({
+            uniforms: {
+                texture: { type:'t', value: 0, texture: baseTexture },
+                palette: { value: palette},
+                paletteSize: { value: 2}
+            },
+            vertexShader: document.getElementById("vertexShader").textContent,
+            fragmentShader: document.getElementById("ditheringShader").textContent,
+            depthWrite: false
+        });
+
+
+        // create quad and add it to orthoscene
+        quad = new THREE.Mesh(new THREE.PlaneGeometry(1, 1), ditherMaterial);
+        quadScene.add(quad);
+
+
+
         // instantiate loader
         var loader = new THREE.OBJLoader();
-
 
         // load resource
         loader.load(
@@ -109,7 +154,7 @@
 
                     // callback function when resource is loaded
                     function (h) {
-                        let count = Math.floor(getRandom(2, 5));
+                        let count = Math.floor(getRandom(4, 8));
                         let heartScl = getRandom(0.01, 0.03);
                         heartList = new Array(count).fill().map(u => {
                             let heart = createHeart(h, radius);
@@ -135,19 +180,16 @@
             }
         );
 
-        // create the renderer and attach it to the DOM
-        renderer = new THREE.WebGLRenderer();
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        renderer.setPixelRatio(window.devicePixelRatio);
-
-        container.appendChild(canvas = renderer.domElement);
 
         //capturer.start();
 
         render();
 
+
+
+        //////////////////// RENDER FUNCTION ////////////////////
+
         function render() {
-            requestAnimationFrame(render);
 
             controls.update();
             var time = (Date.now() - start) * 0.0005;
@@ -158,20 +200,27 @@
                 // each heartobj is {heart, thetafunc, pifunc, radius} 
                 // where heart is the mesh
                 // theta/pi func is function that updates theta/pi on time
-                let { heart, thetafunc, pifunc, radius, light } = heartList[i];
+                let { heart, thetafunc, pifunc, radius, light, offset } = heartList[i];
                 heart.children[0].material.uniforms['lightDir'].value = new THREE.Vector3(light.xfunc(time), light.yfunc(time), light.zfunc(time));
 
                 let { x, y, z } = getPointOnSphere(radius, thetafunc(time), pifunc(time));
 
-                heart.position.set(x, y, z);
+                heart.position.set(x, y - offset, z);
+                heart.rotation.setFromVector3(new THREE.Vector3(light.xfunc(time), light.yfunc(time), light.zfunc(time)));
 
             }
-            renderer.render(scene, camera);
+            renderer.render(scene, camera, baseTexture);
+            quad.material.uniforms['texture'].value = baseTexture.texture;
+            renderer.render(quadScene, orthoCamera);
+
+            requestAnimationFrame(render);
 
             //capturer.capture(canvas);
 
         }
 
+
+        //////////////////// HELPER FUNCTIONS ////////////////////
 
         function getRandom(min, max) {
             return Math.random() * (max - min) + min;
@@ -190,12 +239,13 @@
 
         function createHeart(h, r) {
 
-
             function angleFunction(scl, ang) {
                 return (t) => {
                     return (ang + t * scl) % 2 * Math.PI;
                 }
             }
+
+            let heartSize = 2;
 
             let heart = h.clone();
             scene.add(heart);
@@ -204,13 +254,15 @@
             // destructuring is very cool
             let { x, y, z, theta, pi } = getPointOnSphere(radius);
 
+            let offset = heartSize / 2.0;
+
             let thetafunc = angleFunction(getRandom(0.05, 1.5), theta);
             let pifunc = angleFunction(getRandom(0.05, 1.5), pi);
             let xfunc = lightFunction();
             let yfunc = lightFunction();
             let zfunc = lightFunction();
             let light = { xfunc, yfunc, zfunc };
-            let ho = { heart, thetafunc, pifunc, radius, light };
+            let ho = { heart, thetafunc, pifunc, radius, light, offset };
 
             heart.position.set(x, y, z);
 
